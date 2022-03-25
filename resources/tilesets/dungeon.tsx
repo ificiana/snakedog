08c075bd21b5b6ad83438a6db9ebc99259fb82f0
nopeless <38830903+nopeless@users.noreply.github.com>
2022-03-25 23:40:31 +0900

    replace it with new assets

---
+++ resources/tilesets/dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Tile" tilewidth="16" tileheight="16" tilecount="868" columns="28">
+ <image source="../../../../Users/nOPE/Documents/temp/dungeon_V1.1.2_nyknck/V1.1.1/Tile/Tile.png" width="448" height="500"/>
+</tileset>
